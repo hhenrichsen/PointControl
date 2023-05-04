e15f21d80c4dd66db7ca3fe4e23cee880992c1d1
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-05-04 00:59:46 -0600

    :sparkles: Add utility to read from environment

---
+++ backend/src/service/environment.ts
@@ -0,0 +1,57 @@
+import { Service } from 'typedi';
+
+@Service()
+export class Environment {
+    /**
+     * Get a raw value from the environment. Returns undefined if the value does not exist.
+     *
+     * @param key The key to get from the environment.
+     */
+    public getOpt(key: string): string | undefined;
+
+    /**
+     * Get a parsed value from the environment. Returns undefined if the value does not exist.
+     *
+     * @param key The key to get from the environment.
+     * @param parser The function with which to parse the value.
+     */
+    public getOpt<T>(key: string, parser: (value: string) => T): T | undefined;
+    public getOpt<T>(
+        key: string,
+        parser?: (value: string) => T
+    ): T | string | undefined {
+        const value = process.env[key.toUpperCase()];
+        if (!value) {
+            return undefined;
+        }
+        return parser?.(value) ?? value;
+    }
+
+    /**
+     * Get a raw value from the environment. Throws an error if a value does not exist.
+     *
+     * @param key The key to get from the environment.
+     */
+    public get(key: string): string;
+
+    /**
+     * Get a parsed value from the environment. Throws an error if a value does not exist.
+     *
+     * @param key The key to get from the environment.
+     * @param parser The function with which to parse the value.
+     */
+    public get<T>(key: string, parser: (value: string) => T): T;
+    public get<T>(key: string, parser?: (value: string) => T): T | string {
+        const value = process.env[key.toUpperCase()];
+        if (!value) {
+            throw new Error(
+                `Failed to get ${key.toUpperCase()} from environment.`
+            );
+        }
+        return parser?.(value) ?? value;
+    }
+
+    public isDev() {
+        return this.getOpt('NODE_ENV') === 'DEVELOPMENT';
+    }
+}
