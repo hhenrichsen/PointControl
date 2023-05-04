dc1b88c5431942d7f90ffefaa4fbd2efc22e382d
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-05-04 01:06:38 -0600

    :white_check_mark: Add health check route and test

---
+++ backend/src/routes/health/healthrouter.ts
@@ -0,0 +1,17 @@
+import Router from '@koa/router';
+import { Service } from 'typedi';
+import { GameRepository } from '../../repositories/game.repository';
+import { RouterToken } from '../base';
+
+@Service({ id: RouterToken, multiple: true })
+export class HealthRouter {
+    public readonly router = new Router();
+
+    constructor() {
+        this.router.prefix('/');
+
+        this.router.get('/', async (ctx) => {
+            ctx.body = 'OK';
+        });
+    }
+}
