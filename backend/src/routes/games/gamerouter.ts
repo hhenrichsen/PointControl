bb210ca7656542adb4db31e5717e493dbb780fa5
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-07-02 16:12:03 -0600

    :art: Split request handlers from routes

--- backend/src/routes/games/gamerouter.ts
+++ backend/src/routes/games/gamerouter.ts
@@ -2,16 +2,15 @@ import Router from '@koa/router';
 import { Service } from 'typedi';
 import { GameRepository } from '../../repositories/game.repository';
 import { RouterToken } from '../base';
+import { GameListHandler } from './gamelisthandler';
 
 @Service({ id: RouterToken, multiple: true })
 export class GameRouter {
     public readonly router = new Router();
 
-    constructor(gameQueries: GameRepository) {
+    constructor(gameListHandler: GameListHandler) {
         this.router.prefix('/games');
 
-        this.router.get('/', async (ctx) => {
-            ctx.body = await gameQueries.getGames();
-        });
+        this.router.get('/', gameListHandler.middleware);
     }
 }
--- backend/src/routes/health/healthrouter.ts
+++ backend/src/routes/health/healthrouter.ts
@@ -1,16 +1,15 @@
 import Router from '@koa/router';
 import { Service } from 'typedi';
 import { RouterToken } from '../base';
+import { HealthHandler } from './healthhandler';
 
 @Service({ id: RouterToken, multiple: true })
 export class HealthRouter {
     public readonly router = new Router();
 
-    constructor() {
+    constructor(healthHandler: HealthHandler) {
         this.router.prefix('/');
 
-        this.router.get('/', async (ctx) => {
-            ctx.body = 'OK';
-        });
+        this.router.get('/', healthHandler.middleware);
     }
 }
