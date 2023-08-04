4b3a0878a91ade52e1a90cace787cc199c242800
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-08-04 10:02:17 -0600

    :recycle: Clear the test database when testing

--- scripts/dev-backend-test.sh
+++ scripts/dev-backend-test.sh
@@ -1,1 +1,2 @@
+docker-compose -f devops/docker-compose-dev.yml -p app exec db bash -c "printf '\set AUTOCOMMIT on\ndrop database test; create database test; ' |  psql postgres -U postgres"
 docker-compose -f devops/docker-compose-dev.yml -p app exec tests npm test
